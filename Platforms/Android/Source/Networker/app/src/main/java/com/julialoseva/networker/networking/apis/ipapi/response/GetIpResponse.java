f4b734f77b1699b131a5b669916e8bb5586f0e62
Julia Loseva <julielosseva@gmail.com>
2018-07-15 23:38:20 +0300

    #6 Model class created

---
+++ Platforms/Android/Source/Networker/app/src/main/java/com/julialoseva/networker/networking/apis/ipapi/response/GetIpResponse.java
@@ -0,0 +1,154 @@
+package com.julialoseva.networker.networking.apis.ipapi.response;
+
+public class GetIpResponse {
+
+    private String asNumber;
+
+    public String getAsNumber() {
+        return this.asNumber;
+    }
+
+    public void setAsNumber(String asNumber) {
+        this.asNumber = asNumber;
+    }
+
+    private String city;
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    private String country;
+
+    public String getCountry() {
+        return this.country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    private String countryCode;
+
+    public String getCountryCode() {
+        return this.countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    private String isp;
+
+    public String getIsp() {
+        return this.isp;
+    }
+
+    public void setIsp(String isp) {
+        this.isp = isp;
+    }
+
+    private String latitude;
+
+    public String getLatitude() {
+        return this.latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    private String longitude;
+
+    public String getLongitude() {
+        return this.longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    private String organization;
+
+    public String getOrganization() {
+        return this.organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    private String query;
+
+    public String getQuery() {
+        return this.query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    private String ip;
+
+    public String getIp() {
+        return this.ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    private String region;
+
+    public String getRegion() {
+        return this.region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    private String regionName;
+
+    public String getRegionName() {
+        return this.regionName;
+    }
+
+    public void setRegionName(String regionName) {
+        this.regionName = regionName;
+    }
+
+    private String status;
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    private String timeZone;
+
+    public String getTimeZone() {
+        return this.timeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        this.timeZone = timeZone;
+    }
+
+    private String zipCode;
+
+    public String getZipCode() {
+        return this.zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+}
