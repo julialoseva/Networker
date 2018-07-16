0855df6ca184b9a109e12539696ce2528d04ac33
Julia Loseva <julielosseva@gmail.com>
2018-07-16 21:26:40 +0300

    #11 Transition to HistoryActivity screen and back to MainActivity screen

---
+++ Platforms/Android/Source/Networker/app/src/main/java/com/julialoseva/networker/activities/HistoryActivity.java
@@ -0,0 +1,33 @@
+package com.julialoseva.networker.activities;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.julialoseva.networker.R;
+
+public class HistoryActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle("History");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
--- Platforms/Android/Source/Networker/app/src/main/java/com/julialoseva/networker/activities/MainActivity.java
+++ Platforms/Android/Source/Networker/app/src/main/java/com/julialoseva/networker/activities/MainActivity.java
@@ -1,15 +1,34 @@
 package com.julialoseva.networker.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.julialoseva.networker.R;
 
 public class MainActivity extends Activity {
 
+    private Button historyButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.historyButton = this.findViewById(R.id.history_button);
+        this.historyButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(
+                                MainActivity.this,
+                                HistoryActivity.class
+                        );
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
