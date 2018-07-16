868eafaafb4a629543d7bd734be56c6d5d1a573b
Julia Loseva <julielosseva@gmail.com>
2018-07-16 21:49:35 +0300

    #9 Updated current IP

--- Platforms/Android/Source/Networker/app/src/main/java/com/julialoseva/networker/activities/MainActivity.java
+++ Platforms/Android/Source/Networker/app/src/main/java/com/julialoseva/networker/activities/MainActivity.java
@@ -3,19 +3,39 @@ package com.julialoseva.networker.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.julialoseva.networker.R;
+import com.julialoseva.networker.networking.apis.ipapi.client.IpApiClient;
+import com.julialoseva.networker.networking.apis.ipapi.response.GetIpResponse;
 
 public class MainActivity extends Activity {
 
+    private SwipeRefreshLayout swipeRefreshLayout;
+
     private Button historyButton;
+
+    private TextView ipTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.swipeRefreshLayout = this.findViewById(R.id.swipe_refresh_layout);
+        this.swipeRefreshLayout.setOnRefreshListener(
+                new SwipeRefreshLayout.OnRefreshListener() {
+                    @Override
+                    public void onRefresh() {
+                        updateIPAddress();
+                    }
+                }
+        );
 
         this.historyButton = this.findViewById(R.id.history_button);
         this.historyButton.setOnClickListener(
@@ -30,5 +50,29 @@ public class MainActivity extends Activity {
                     }
                 }
         );
+
+        this.ipTextView = this.findViewById(R.id.ip_text_view);
+    }
+
+    public void updateIPAddress() {
+        new IpApiClient().getIp(new IpApiClient.OnChangeListener() {
+            @Override
+            public void onStarted() {
+            }
+
+            @Override
+            public void onSuccess(GetIpResponse response) {
+                ipTextView.setText(response.getQuery().toString());
+            }
+
+            @Override
+            public void onFailed() {
+                Toast.makeText(
+                        MainActivity.this,
+                        "Error",
+                        Toast.LENGTH_LONG
+                );
+            }
+        });
     }
 }
