de491ac66b85c77d9cdc5152be3e0884c5f198b9
Julia Loseva <julielosseva@gmail.com>
2018-07-17 23:21:26 +0300

    #13 Added new text to IP text view

--- Platforms/Android/Source/Networker/app/src/main/java/com/julialoseva/networker/activities/MainActivity.java
+++ Platforms/Android/Source/Networker/app/src/main/java/com/julialoseva/networker/activities/MainActivity.java
@@ -72,6 +72,7 @@ public class MainActivity extends Activity {
     }
 
     private void beforeCurrentIpRequest() {
+        updateIpTextView("Updating...");
     }
 
     private void afterCurrentIpRequestResult(GetIpResponse response, boolean success) {
