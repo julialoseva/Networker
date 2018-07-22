65f0caa396c63bea7e03bfda3a9e5c57cf476aa9
Julia Loseva <julielosseva@gmail.com>
2018-07-22 13:55:36 +0300

    #24 Implemented default settings for Realm

--- Platforms/Android/Source/Networker/app/src/main/java/com/julialoseva/networker/app/NetworkerApp.java
+++ Platforms/Android/Source/Networker/app/src/main/java/com/julialoseva/networker/app/NetworkerApp.java
@@ -2,5 +2,20 @@ package com.julialoseva.networker.app;
 
 import android.app.Application;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 public class NetworkerApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Realm.init(this);
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
+                .schemaVersion(1)
+                .build();
+
+        Realm.setDefaultConfiguration(realmConfiguration);
+    }
 }
